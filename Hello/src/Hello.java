06369cc473805881608304a01a7622d3d089d21a
BaeSyoon <soyoon22@gmail.com>
2024-08-22 17:28:25 +0900

    Hello Java

---
+++ Hello/src/Hello.java
@@ -0,0 +1,17 @@
+
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("Hello, World!!");
+		System.out.println("Hello, World!!");
+	}
+}
+
+class Ex2_1 {
+	public static void main(String args[]) {
+		System.out.println("Second Class");
+		int x = 5;
+		System.out.println(x);
+		
+	}
+}
